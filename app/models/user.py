9367407c45561bb912f7752eaa61ad27b9ce2b49
Sandmish123 <sandeepmishra991056@gmail.com>
2026-03-12 02:28:26 +0530

    User.py updated Modified the name as Nullable

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=True)
     phone = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=True)
     dob = Column(Date, nullable=True)
